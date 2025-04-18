ab4734255bf1d97a21f42e3ba45f4905580858c8
sujeets23 <sujeetsattigeri@gmail.com>
2025-04-18 23:13:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Graphic Designer", "UI/UX Designer"],
+        strings: ["Graphic Designer", "Video Editor", "UI/UX Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
